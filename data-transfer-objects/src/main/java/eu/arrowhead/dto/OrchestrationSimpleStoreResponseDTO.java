b65a993f36e05a1f3f30d77555089f2abff9a2f6
Katinka666 <jako.katinka@gmail.com>
2025-11-27 01:33:17 +0100

    add headers

--- data-transfer-objects/src/main/java/eu/arrowhead/dto/OrchestrationSimpleStoreResponseDTO.java
+++ data-transfer-objects/src/main/java/eu/arrowhead/dto/OrchestrationSimpleStoreResponseDTO.java
@@ -1,3 +1,19 @@
+/*******************************************************************************
+ *
+ * Copyright (c) 2025 AITIA
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ *
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *  	AITIA - implementation
+ *  	Arrowhead Consortia - conceptualization
+ *
+ *******************************************************************************/
 package eu.arrowhead.dto;
 
 public record OrchestrationSimpleStoreResponseDTO(
@@ -5,7 +21,7 @@ public record OrchestrationSimpleStoreResponseDTO(
 		String consumer,
 		String serviceDefinition,
 		String serviceInstanceId,
-		Integer Priority,
+		Integer priority,
 		String createdBy,
 		String updatedBy,
 		String createdAt,
